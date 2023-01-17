cbac93d164ffb5a9e7128feab5b9986600e4f9ae
Muhammad Waqar <MW6069605@GMAIL.COM>
2023-01-17 17:45:04 +0500

    added school contract working

--- contracts/School.sol
+++ contracts/School.sol
@@ -2,5 +2,19 @@
 pragma solidity ^0.8.9;
 
 contract School{
-    
+    string coursename;
+    address teacher;
+    address owner;
+    uint256 baseterm;
+    mapping (uint256=>coursename) courses;
+    constructor(){
+        owner=msg.sender;
+        baseterm=90;
+    }
+
+    modifier onlyteacher() {
+        require(msg.sender == teacher);
+        _;
+    }
+
 }
---
+++ contracts/certificate.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+contract certificate is ERC721,ERC721Me{
+    constructor() ERC721("QTKN","QTKN"){        
+    }
+
+
+}
