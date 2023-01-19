f390cff5b9a50a3e04de6215a790dc45af04ea0d
Muhammad Waqar <MW6069605@GMAIL.COM>
2023-01-19 16:40:50 +0500

    school contract half

--- contracts/QTKN.sol
+++ contracts/QTKN.sol
@@ -4,21 +4,55 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 interface QTKN_ERC20 {
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-    function transferToken(address to, uint256 value) external payable;
     event Transfer(address indexed from, address indexed to, uint256 value);
+
+    function totalSupply() external view returns (uint256);
+    
+    function balanceOf(address account) external view returns (uint256);
+    
+    function transfer(address to, uint256 value) external payable;
+
 }
 
-abstract contract QTKN is QTKN_ERC20{
-    string public name='QTKN Token';
-    string public symbol='QTKN';
-    uint256 public _totalSupply;
-    address public owner;
-    //mapping for the balances
-    mapping(address => uint256) public balances;
 
-    constructor() {
+interface IERMetaData is QTKN_ERC20{
+    function name() external view returns (string memory);
+
+    function symbol() external view returns (string memory);
+}
+
+abstract contract context {
+    function _msgSender() internal view virtual returns (address){
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+abstract contract QTKN is QTKN_ERC20,context,IERMetaData{
+    //mapping for the balances
+    mapping(address => uint256) private balances;
+
+    //mapping for allowance
+    mapping(address => mapping(address => uint256)) private 
+    _allowance;
+    //coin name
+    string public name='QTKN Token';
+    //coin symbol
+    string public symbol='QTKN';
+    //coin supply
+    uint256 private _totalSupply;
+    //owner of the coin
+    address public owner;
+
+
+function checkBalance() public view returns (uint256) {
+    return address(this).balance;
+}
+
+    constructor(uint256 total) {
+        _totalSupply=total;
         balances[msg.sender]=_totalSupply;
         owner=msg.sender;
     }
@@ -27,7 +61,11 @@ abstract contract QTKN is QTKN_ERC20{
         return _totalSupply;
     }
 
-    function transferToken(address _to) public payable{
+    function balanceOf(address _account) public view returns    (uint256){
+        return balances[_account];
+    }
+
+    function transfer(address _to) public payable{
         require(balances[msg.sender]>=100);
         require(msg.value>=1 ether); 
         balances[msg.sender] -=100;
@@ -35,12 +73,10 @@ abstract contract QTKN is QTKN_ERC20{
         emit Transfer(msg.sender, _to, 100);
     }
 
-    function balanceOf(address _account) public view returns(uint256){
-        return balances[_account];
+    function mint(address to, uint256 value)  public {
+        require(msg.sender==owner);
+        balances[to]+=value;
+        _totalSupply+=value;
+        emit Transfer(address(0), to, value);
     }
-
-    // function _mint(address to,uint256 value) public {
-    //     require(balances[msg.sender]>=value);
-
-    // }
 }
--- contracts/School.sol
+++ contracts/School.sol
@@ -1,19 +1,86 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
-contract School{
-    string coursename;
-    address teacher;
-    address owner;
-    uint256 baseterm;
-    constructor(){
+contract School {
+    address public owner;
+    uint256 private baseterm;
+    uint256 private tax=3;
+    //events
+    event NewCourse(address indexed teacher,uint256 courseID, uint256 price);
+    event Newteacher(address indexed teacher);
+    event Changebaseterm(uint256 newterm);
+    //mapping for teacher 
+    mapping (address=>bool) teacher;
+
+    //mapping for the course and teacher assign
+    mapping (address=> mapping(uint256=>address)) public courses;
+
+    //mapping teacher of course
+    mapping(uint256=>address) public teachersofcourse;
+
+    //mapping teacher share
+    mapping(uint256=>uint256) public teachershare; 
+    //course price mapping
+    mapping (uint256=>uint256) public courseprice;
+
+    //school share mapping
+    mapping (uint256=>uint256) public schoolshare;
+
+    //mapping for course and link
+    mapping (uint256=>string) links;
+
+
+    constructor (){
         owner=msg.sender;
         baseterm=90;
+        }
+
+    function createCourse(address _teacher,uint256 _courseId, uint _basePrice, uint _teacherShare,string memory courselink) public {
+        require(teacher[_teacher] == true, "You are not a registered teacher");
+         require(courses[_teacher][_courseId] == _teacher, "Course already exists for this");
+         require(_teacherShare <= baseterm, "Teacher share must be less than or equal to the base term set by the school"); 
+         links[_courseId]=courselink;
+         uint _schoolShare = baseterm - _teacherShare;
+         uint _price = (_basePrice * 3 / 100)+_basePrice+_schoolShare;
+         courseprice[_courseId] = _price;
+         teachershare[_courseId] = _teacherShare;
+         schoolshare[_courseId] = _schoolShare;
+         teachersofcourse[_courseId] = _teacher;
+         courses[_teacher][_courseId] = _teacher;
+         emit NewCourse(_teacher, _courseId, courseprice[_courseId]);
+    }
+    
+    function  addTeacher(address _name) public{
+     require(msg.sender==owner,"Only owner can add teacher");
+     require(_name==address(0),"Invalid Address");
+     require(teacher[_name]==false,'Teacher Already Exist');
+     teacher[_name]=true;
+     emit Newteacher(_name);
     }
 
-    modifier onlyteacher() {
-        require(msg.sender == teacher);
-        _;
+    function changebaseterm(uint256 value)public {
+        require(msg.sender==owner,"Only owner can change the base terms");
+        baseterm=value;
+        emit Changebaseterm(value);
     }
+    function getCoursePrice(uint256 _courseId) public view returns (uint) {
+    return courseprice[_courseId];
+    }
+
+    function getTeacherShare(uint256 _courseId) public view returns (uint) {
+        return teachershare[_courseId];
+    }
+
+    function getSchoolShare(uint256 _courseId) public view returns (uint) {
+        return schoolshare[_courseId];
+    }
+
+    function getTeacherOfCourse(uint256 _courseId) public view returns (address) {
+        return teachersofcourse[_courseId];
+    }
+     
+     function BuyCourse(uint256 _courseID, uint256 _pay) public payable {
+        
+     }
 
 }
